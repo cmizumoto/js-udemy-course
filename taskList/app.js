8811251ea63bad9fdda77364ff04864b90d81209
Caio Mizumoto <665112@liq.com.br>
2020-02-26 10:51:27 -0300

    app js update

--- taskList/app.js
+++ taskList/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Function to load all event listeners
 function loadEventListeners() {
+    // DOM Load Event
+    document.addEventListener('DOMContentLoaded', getTasks);
     // add task event
     form.addEventListener('submit', addTask);
     // remove task event
@@ -20,6 +22,38 @@ function loadEventListeners() {
     filter.addEventListener('keyup', filterTasks);
 }
 
+// Get Tasks from local storage
+function getTasks(){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach( function (task) {
+        // Create li element
+        const li = document.createElement('li');
+        // add class
+        li.className = 'collection-item';
+        // create text node and append to li
+        li.appendChild(document.createTextNode(task));
+        // create new link element
+        const link = document.createElement('a');
+        // add class
+        link.className = 'delete-item secondary-content';
+        // add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>'
+        // Append the link to li
+        li.appendChild(link);
+
+        // append li to ul
+        taskList.appendChild(li);
+    });
+}
+
+// Add Tasks
 function addTask(event){
     if(taskInput.value === ''){
         return alert('add a task');
@@ -43,20 +77,60 @@ function addTask(event){
     // append li to ul
     taskList.appendChild(li);
 
+    // Store in Local Storage
+    storeTaskInLocalStorage(taskInput.value);    
+
     // Clear the input
     taskInput.value = '';
 
-
     event.preventDefault();
 }
+
+// Store Task
+function storeTaskInLocalStorage(task){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        // Local storage only stores strings
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    // Local storage only stores strings
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 
 // Remove task
 function removeTask(event) {
     if(event.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are you sure?')){
             event.target.parentElement.parentElement.remove();
+
+            // Remove from Local Storage
+            removeTaskFromLocalStorage(event.target.parentElement.parentElement);
+
         }
     }
+}
+
+// Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear Tasks
@@ -69,6 +143,14 @@ function clearTasks(){
     }
 
     // https://jsperf.com/innerhtml-vs-removechild/47
+
+    // Clear from Local Storage
+    clearTasksFromLocalStorage();
+}
+
+// Clear tasks from local storage
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
 }
 
 // Filter Tasks
