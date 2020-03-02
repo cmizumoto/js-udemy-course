d73bf3020a9e3c7a637d85d0ac47edc9ecb25e93
Caio Mizumoto <665112@liq.com.br>
2020-03-02 18:05:58 -0300

    =update app.js with functionalities, missing visuals

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -11,7 +11,48 @@ function calculateResults(event){
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
 
-    
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed();
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        showError('Please check your numbers');
+    }
 
     event.preventDefault();
+}
+
+// Show error
+function showError(error){
+    // Create a Div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // add Class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+// Clear Error
+function clearError(){
+    document.querySelector('.alert').remove();
 }
