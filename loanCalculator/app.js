271a7f2e21f589b8462c6546e4c5c0d0ca3ae68b
Caio Mizumoto <665112@liq.com.br>
2020-03-03 10:24:56 -0300

    =finish app.js functionalities

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -1,8 +1,18 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(event){
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+    
+    setTimeout(calculateResults, 2000);
+
+    event.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(event){
+function calculateResults(){
     // UI Vars
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -23,15 +33,26 @@ function calculateResults(event){
         monthlyPayment.value = monthly.toFixed();
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show results
+        document.getElementById('results').style.display = 'block';
+
+        // Hide loader
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
 
-    event.preventDefault();
 }
 
 // Show error
 function showError(error){
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
+
     // Create a Div
     const errorDiv = document.createElement('div');
 
