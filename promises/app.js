f5cf450f105c73407d39e7032d3943a1a5bf0014
Caio Mizumoto <665112@liq.com.br>
2020-03-13 09:54:36 -0300

    add promises file

---
+++ promises/app.js
@@ -0,0 +1,37 @@
+const posts = [
+    {title: 'Post one', body: 'this is post one'},
+    {title: 'Post two', body: 'this is post two'}
+];
+
+
+function createPost(post){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            posts.push(post);
+
+            const error = false;
+            
+            if(!error){
+                resolve();
+            } else {
+                reject('Error: something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+function getPost(){
+    setTimeout(function (){
+        let output = '';
+        posts.forEach(function(post) {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+createPost({title: 'Post three', body: 'this is post three'})
+.then(getPost)
+.catch(function(err){
+    console.log(err);
+});
