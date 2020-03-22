7fcc4a94482d1c02e5075707048b88373eda6ffe
Caio Mizumoto <caio.mizumoto@gmail.com>
2020-03-22 11:17:01 -0300

    fix typo in customHttpLibrary folder and add customHttpLibrary-2 folder featuring promises

---
+++ customHttpLibrary-2/easyHttp2.js
@@ -0,0 +1,69 @@
+/* 
+EasyHTTP Library
+Library for making HTTP request
+
+@version 2.0.0
+@author Caio Mizumoto
+@license MIT
+*/
+
+class EasyHTTP{
+    // Make an HTTP GET request
+    get(url){
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // Make Http POST request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // Make Http PUT request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+
+    // Make an HTTP DELETE Request
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(() => resolve('Resource Deleted...'))
+                .catch(err => reject(err));
+        });
+    }
+}
+
