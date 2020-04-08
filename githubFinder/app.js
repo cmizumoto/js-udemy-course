dce47989148fc227bc7548feab011494f8c7612c
Caio Mizumoto <caio.mizumoto@gmail.com>
2020-04-08 13:14:07 -0300

    add get repo functionality to app ui and github js

--- githubFinder/app.js
+++ githubFinder/app.js
@@ -1,5 +1,7 @@
 // Init GitHub
 const github = new Github;
+// init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -14,14 +16,17 @@ searchUser.addEventListener('keyup', (e) => {
         .then(data => {
             if(data.profile.message === 'Not Found') {
                 // Show alert
+                ui.showAlert('User not found', 'alert alert-danger');
 
             } else {
                 // Show profile
-
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
             }
         })
     } else {
         //  Clear profile
+        ui.clearProfile();
     }
 
 });
--- githubFinder/github.js
+++ githubFinder/github.js
@@ -2,15 +2,21 @@ class Github {
     constructor() {
         this.client_id = '31016b4ee2d3d934bf76',
         this.client_secret ='e72251fd4531c081b50a1afe15f92cca71c676c9'
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile
+            profile,
+            repos
         }
     }
 }
